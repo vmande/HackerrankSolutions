4ae0ffca375dfc1a9299c6415bd117d91d323b95
vmande <vmande94@gmail.com>
2019-12-28 18:20:18 -0500

    Simple Array Sum

---
+++ SimpleArraySum.java
@@ -0,0 +1,39 @@
+public class SimpleArraySum {
+
+  /*
+   * Complete the simpleArraySum function below.
+   */
+  static int simpleArraySum(int[] arr) {
+    int i=0;
+    int sum=0;
+    while(i < arr.length){
+      sum = sum + arr[i];
+      i++;
+    }
+    return sum;
+  }
+
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static void main(String[] args) throws IOException {
+    BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+    int arCount = Integer.parseInt(scanner.nextLine().trim());
+
+    int[] ar = new int[arCount];
+
+    String[] arItems = scanner.nextLine().split(" ");
+
+    for (int arItr = 0; arItr < arCount; arItr++) {
+      int arItem = Integer.parseInt(arItems[arItr].trim());
+      ar[arItr] = arItem;
+    }
+
+    int result = simpleArraySum(ar);
+
+    bufferedWriter.write(String.valueOf(result));
+    bufferedWriter.newLine();
+
+    bufferedWriter.close();
+  }
+}
